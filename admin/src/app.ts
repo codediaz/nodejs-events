bc0569aa5b00471b322f5a2f48b64958357edaba
Sergio Díaz <sdiazf99@outlook.com>
2023-07-28 08:47:54 -0500

    Feature: First Request - Get and Post with uri : http://localhost:8000/api/products

--- admin/src/app.ts
+++ admin/src/app.ts
@@ -1,10 +1,14 @@
 import * as express from 'express';
+import {Request, Response} from "express";
 import * as cors from 'cors';
 
 import {createConnection} from "typeorm";
+import {Product} from "./entity/product";
 
 
 createConnection().then(db => {
+    const productRepository = db.getRepository(Product)
+
     const app = express()
 
     app.use(cors({
@@ -12,6 +16,20 @@ createConnection().then(db => {
     }))
 
     app.use(express.json())
+
+    app.get('/api/products', async (req: Request, res: Response) => {
+        const products = await productRepository.find()
+        res.json(products)
+
+
+    })
+
+    app.post('/api/products', async (req: Request, res: Response) => {
+        const product =  productRepository.create(req.body)
+        const result = await productRepository.save(product);
+
+        return res.send(result)
+    })
 
     console.log('Listening to port: 8000')
     app.listen(8000)
